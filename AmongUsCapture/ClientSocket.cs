28d3fd8a71d4b9421a244f93bf9ae21a5ed396ce
Carbon <robotcreeper13@gmail.com>
2020-09-21 15:53:34 -0500

    Minor Async -> Sync conversions. Makes code wait before continuing.

--- AmongUsCapture/ClientSocket.cs
+++ AmongUsCapture/ClientSocket.cs
@@ -26,7 +26,7 @@ namespace AmongUsCapture
                 UpdateGuildID(paramGuildID, false);
             };
 
-            socket.ConnectAsync();
+            socket.ConnectAsync().GetAwaiter().GetResult(); // Make code wait for socket to fully connect before trying to send stuff.
 
             while(true)
             {
@@ -50,12 +50,12 @@ namespace AmongUsCapture
 
         private void GameStateChangedHandler(object sender, GameStateChangedEventArgs e)
         {
-            socket.EmitAsync("state", JsonSerializer.Serialize(e.NewState)); // could possibly use continueWith() w/ callback if result is needed
+            socket.EmitAsync("state", JsonSerializer.Serialize(e.NewState)).GetAwaiter().GetResult(); // could possibly use continueWith() w/ callback if result is needed
         }
 
         private void PlayerChangedHandler(object sender, PlayerChangedEventArgs e)
         {
-            socket.EmitAsync("player", JsonSerializer.Serialize(e));
+            socket.EmitAsync("player", JsonSerializer.Serialize(e)).GetAwaiter().GetResult(); //Makes code wait for socket to emit before closing thread.
         }
     }
 }

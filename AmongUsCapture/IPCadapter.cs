d75ae15339e2f7a8271cd5634a122dcb17700888
Carbon <robotcreeper13@gmail.com>
2020-10-13 13:35:03 -0500

    Refactor IPCAdapter and add the appropriate methods.

--- AmongUsCapture/IPCadapter.cs
+++ AmongUsCapture/IPCadapter.cs
@@ -1,99 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Web;
+using Microsoft.Win32;
 using Newtonsoft.Json;
+using SharedMemory;
 
 namespace AmongUsCapture
 {
     class IPCadapter
     {
+        public const string appName = "AmongUsCapture";
+        private const string UriScheme = "aucapture";
+        private const string FriendlyName = "AmongUs Capture";
+        private Mutex mutex;
         private static IPCadapter instance = new IPCadapter();
         public event EventHandler<StartToken> OnToken;
         public static IPCadapter getInstance()
         {
             return instance;
         }
-        public void RunLoop(string initialURI)
+
+        public URIStartResult HandleURIStart(string[] args)
         {
-            if(initialURI != null)
+            var myProcessId = Process.GetCurrentProcess().Id;
+            //Process[] processes = Process.GetProcessesByName("AmongUsCapture");
+            //foreach (Process p in processes)
+            //{
+            //if (p.Id != myProcessId)
+            //    {
+            //        p.Kill();
+            //    }
+            // }
+            Console.WriteLine(Program.GetExecutablePath());
+
+            mutex = new Mutex(true, appName, out var createdNew);
+            var wasURIStart = args.Length > 0 && args[0].StartsWith(UriScheme + "://");
+            var result = URIStartResult.CONTINUE;
+
+            if (!createdNew) // send it to already existing instance if applicable, then close
             {
-                OnToken?.Invoke(this, StartToken.FromString(initialURI));
+                if (wasURIStart) SendToken(args[0]);
+
+                return URIStartResult.CLOSE;
             }
-            while (false)
+            else if (wasURIStart) // URI start on new instance, continue as normal but also handle current argument
             {
-                PipeSecurity ps = new PipeSecurity();
+                result = URIStartResult.PARSE;
+            }
 
-                ps.AddAccessRule(new PipeAccessRule("Users", PipeAccessRights.FullControl, System.Security.AccessControl.AccessControlType.Allow));
-                
-                NamedPipeServerStream pipeServer = NamedPipeServerStreamConstructors.New("AmongUsCapturePipe", PipeDirection.InOut, 1, pipeSecurity: ps);
+            RegisterProtocol();
 
-                pipeServer.WaitForConnection();
-                Console.WriteLine("Client connected");
-                try
-                { 
-                    //Read the request from the client. Once the client has
-                    // written to the pipe its security token will be available.
-                    StreamString ss = new StreamString(pipeServer);
+            return result;
+        }
 
-                    string rawToken = ss.ReadString();
-                    StartToken startToken = StartToken.FromString(rawToken);
-                    Console.WriteLine($@"Decoded message as {JsonConvert.SerializeObject(startToken, Formatting.Indented)}");
-                    OnToken?.Invoke(this, startToken);
-                }
-                // Catch the IOException that is raised if the pipe is broken
-                // or disconnected.
-                catch (IOException e)
+        public bool SendToken(string jsonText)
+        {
+            var rpcMaster = new RpcBuffer(appName);
+            var RPCresult = rpcMaster.RemoteRequest(Encoding.UTF8.GetBytes(jsonText));
+            var SlaveResponse = Encoding.UTF8.GetString(RPCresult.Data, 0, RPCresult.Data.Length);
+            return RPCresult.Success;
+        }
+
+        private static void RegisterProtocol()
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Classes\\" + UriScheme))
+            {
+                // Replace typeof(App) by the class that contains the Main method or any class located in the project that produces the exe.
+                // or replace typeof(App).Assembly.Location by anything that gives the full path to the exe
+                var applicationLocation = Program.GetExecutablePath();
+
+                key.SetValue("", "URL:" + FriendlyName);
+                key.SetValue("URL Protocol", "");
+
+                using (var defaultIcon = key.CreateSubKey("DefaultIcon"))
                 {
-                    Console.WriteLine(@"ERROR: {0}", e.Message);
+                    defaultIcon.SetValue("", applicationLocation + ",1");
                 }
-                pipeServer.Close();
+
+                using (var commandKey = key.CreateSubKey(@"shell\open\command"))
+                {
+                    commandKey.SetValue("", "\"" + applicationLocation + "\" \"%1\"");
+                }
             }
         }
-    }
-    public class StreamString
-    {
-        private Stream ioStream;
-        private UnicodeEncoding streamEncoding;
 
-        public StreamString(Stream ioStream)
+        public void RegisterSlave()
         {
-            this.ioStream = ioStream;
-            streamEncoding = new UnicodeEncoding();
-        }
-
-        public string ReadString()
-        {
-            int len = 0;
-
-            len = ioStream.ReadByte() * 256;
-            len += ioStream.ReadByte();
-            byte[] inBuffer = new byte[len];
-            ioStream.Read(inBuffer, 0, len);
-
-            return streamEncoding.GetString(inBuffer);
-        }
-
-        public int WriteString(string outString)
-        {
-            byte[] outBuffer = streamEncoding.GetBytes(outString);
-            int len = outBuffer.Length;
-            if (len > UInt16.MaxValue)
+            var rpcSlave = new RpcBuffer(appName, (msgId, payload) =>
             {
-                len = (int)UInt16.MaxValue;
-            }
-            ioStream.WriteByte((byte)(len / 256));
-            ioStream.WriteByte((byte)(len & 255));
-            ioStream.Write(outBuffer, 0, len);
-            ioStream.Flush();
+                var serverResponse = "Carbon has a huge pp also this is debug messages.";
+                var gotData = Encoding.UTF8.GetString(payload, 0, payload.Length);
+                Console.WriteLine($"RPCSlave: Got data: {gotData}");
+                OnToken?.Invoke(this, StartToken.FromString(gotData)); //Invoke method and return.
+                return Encoding.UTF8.GetBytes(serverResponse);
+            });
+        }
 
-            return outBuffer.Length + 2;
+        public void startWithToken(string uri)
+        {
+            OnToken?.Invoke(this, StartToken.FromString(uri));
         }
     }
+
+    public enum URIStartResult
+    {
+        CLOSE,
+        PARSE,
+        CONTINUE
+    }
+
     public class StartToken : EventArgs
     {
         public string Host { get; set; }

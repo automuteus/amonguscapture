0b74553d02ab4bed23833f0b3ecccd03b4151723
litetex <litetex.dev@gmail.com>
2020-10-31 16:13:45 +0100

    Revert "Removed console project"
    This reverts commit ba0a57c2952b5cb5b1f6ef39e835f0ed405b979d.

---
+++ AUCapture-Console/IPC/IPCAdapter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using AUCapture_Console.IPC.RpcBuffer;
+
+namespace AUCapture_Console.IPC
+{
+    abstract class IPCAdapter
+    {
+        public const string appName = "AmongUsCapture";
+        protected const string UriScheme = "aucapture";
+        protected const string FriendlyName = "AmongUsCapture";
+        protected Mutex mutex;
+        private static IPCAdapter instance;
+
+        public static IPCAdapter getInstance()
+        {
+            return instance ??= new IPCAdapterRpcBuffer();
+        }
+        public event EventHandler<StartToken> OnToken;
+        protected virtual void OnTokenEvent(StartToken e)
+        {
+            // Safely raise the event for all subscribers
+            OnToken?.Invoke(this, e);
+        }
+        
+        public abstract URIStartResult HandleURIStart(string[] args);
+        public abstract Task<bool> SendToken(string jsonText);
+        public abstract Task SendToken(string host, string connectCode);
+        public abstract Task RegisterMinion();
+        public abstract Task StartWithToken(string uri);
+
+        public virtual Task Cancel()
+        {
+            return Task.CompletedTask;
+        }
+    }
+    
+    
+    public enum URIStartResult
+    {
+        CLOSE,
+        PARSE,
+        CONTINUE
+    }
+
+    public class StartToken : EventArgs
+    {
+        public string Host { get; set; }
+        public string ConnectCode { get; set; }
+
+        public static StartToken FromString(string rawToken)
+        {
+            try
+            {
+                rawToken = new string(rawToken.Where(c => !char.IsControl(c)).ToArray());
+                Uri uri = new Uri(rawToken);
+                NameValueCollection nvc = HttpUtility.ParseQueryString(uri.Query);
+                bool insecure = (nvc["insecure"] != null && nvc["insecure"] != "false") || uri.Query == "?insecure";
+                return new StartToken() { Host = (insecure ? "http://" : "https://") + uri.Authority, ConnectCode = uri.AbsolutePath.Substring(1) };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return new StartToken();
+            }
+        }
+    }
+}
---
+++ AUCapture-Console/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using AmongUsCapture;
+using AmongUsCapture.TextColorLibrary;
+using AUCapture_Console.IPC;
+
+namespace AUCapture_Console
+{
+    class Program
+    {
+        public static readonly ClientSocket socket = new ClientSocket();
+        
+        public static void OnTokenHandler(object sender, StartToken token)
+        {
+            Settings.conInterface.WriteModuleTextColored("ClientSocket", Color.Cyan,
+                $"Attempting to connect to host {Color.LimeGreen.ToTextColor()}{token.Host}{Color.White.ToTextColor()} with connect code {Color.Red.ToTextColor()}{token.ConnectCode}{Color.White.ToTextColor()}");
+            socket.Connect(token.Host, token.ConnectCode);
+        }
+        public static string GetExecutablePath()
+        {
+            return Process.GetCurrentProcess().MainModule.FileName;
+        }
+
+        static void Main(string[] args)
+        {
+
+            var uriStart = IPCAdapter.getInstance().HandleURIStart(args);
+            
+            switch (uriStart)
+            {
+                case URIStartResult.CLOSE:
+                    Environment.Exit(0);
+                    break;
+                case URIStartResult.PARSE:
+                    Console.WriteLine($"Starting with args : {args[0]}");
+                    break;
+                case URIStartResult.CONTINUE:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            var foreverTask = Task.Factory.StartNew(() =>
+            {
+                IPCAdapter.getInstance().OnToken += OnTokenHandler;
+                var socketTask = Task.Factory.StartNew(() => socket.Init()); // run socket in background. Important to wait for init to have actually finished before continuing
+                GameMemReader.getInstance().GameStateChanged += GameStateChangedHandler;
+                GameMemReader.getInstance().PlayerChanged += UserForm_PlayerChanged;
+                GameMemReader.getInstance().ChatMessageAdded += OnChatMessageAdded;
+                GameMemReader.getInstance().JoinedLobby += OnJoinedLobby;
+                var gameReader = Task.Factory.StartNew(() => GameMemReader.getInstance().RunLoop()); // run loop in background
+                
+                socketTask.Wait();
+                IPCAdapter.getInstance().RegisterMinion();
+                Settings.conInterface = new ConsoleLogger();
+                if (uriStart == URIStartResult.PARSE) IPCAdapter.getInstance().SendToken(args[0]);
+                gameReader.Wait();
+            });
+            foreverTask.Wait();
+        }
+
+        private static void OnJoinedLobby(object? sender, LobbyEventArgs e)
+        {
+            AmongUsCapture.Settings.conInterface.WriteModuleTextColored("Join", Color.DarkKhaki, $"Joined lobby: {e.LobbyCode}");
+        }
+
+        private static void OnChatMessageAdded(object? sender, ChatMessageEventArgs e)
+        {
+            AmongUsCapture.Settings.conInterface.WriteModuleTextColored("CHAT", Color.DarkKhaki, $"{e.Sender}: {e.Message}");
+        }
+
+        private static void UserForm_PlayerChanged(object? sender, PlayerChangedEventArgs e)
+        {
+            Settings.conInterface.WriteModuleTextColored("PlayerChange", Color.DarkKhaki, $"{e.Name}: {e.Action}");
+
+        }
+
+        private static void GameStateChangedHandler(object? sender, GameStateChangedEventArgs e)
+        {
+            Settings.conInterface.WriteModuleTextColored("GameMemReader", Color.Lime, $"State changed to {Color.Cyan.ToTextColor()}{e.NewState}");
+        }
+    }
+}

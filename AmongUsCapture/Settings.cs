71b5ab2a946c094ebb598613f2c2791bf81b6b18
Carbon <robotcreeper13@gmail.com>
2020-11-24 17:52:19 -0600

    Put the default URL in the right place after merge.

--- AmongUsCapture/Settings.cs
+++ AmongUsCapture/Settings.cs
@@ -28,7 +28,7 @@ namespace AmongUsCapture
         [Option(Alias = "DebugConsole", DefaultValue = false)]
         bool debugConsole { get; set; }
 
-        [Option(Alias = "IndexURL", DefaultValue = "https://raw.githubusercontent.com/denverquane/amonguscapture/OffsetManager/Offsets.json")]
+        [Option(Alias = "IndexURL", DefaultValue = "https://raw.githubusercontent.com/denverquane/amonguscapture/master/Offsets.json")]
         string IndexURL { get; set; }
         
     }

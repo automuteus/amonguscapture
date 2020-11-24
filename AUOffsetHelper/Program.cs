0f1e4a2dd98000f267d6c462d5b5239e1e19f34f
Carbon <robotcreeper13@gmail.com>
2020-11-24 14:41:07 -0600

    Add the new and old offsets

--- AUOffsetHelper/Program.cs
+++ AUOffsetHelper/Program.cs
@@ -8,21 +8,21 @@ namespace AUOffsetHelper
 {
     static class Program
     {
-        public static string hash = "FF1DAE62454312FCE09A39061999C26FD26440FDA5F36C1E6424290A34D05B08";
-        public static string description = "v2020.11.17s (Release 2)";
+        public static string hash = "38119B8551718D9016BAFEEDC105610D5B3AED5B0036D1A6060B8E2ABE523C02";
+        public static string description = "v2020.11.17s (Release 1)";
 
-        public static int AmongUsClientOffset = 0x143BE9C;
+        public static int AmongUsClientOffset = 0x14B2C9C;
 
-        public static int GameDataOffset = 0x143BF38;
+        public static int GameDataOffset = 0x14B2E9C;
 
-        public static int MeetingHudOffset = 0x143BBB4;
+        public static int MeetingHudOffset = 0x14B2A7C;
 
-        public static int GameStartManagerOffset = 0x1399BD0;
+        public static int GameStartManagerOffset = 0x13983DC;
 
-        public static int HudManagerOffset = 0x1060AC0;
+        public static int HudManagerOffset = 0x138B9FC;
 
 
-        public static int ServerManagerOffset = 0x138FCD0;
+        public static int ServerManagerOffset = 0x138E51C;
 
         public static int WinDataOffset = 0x14B2AFC;
 

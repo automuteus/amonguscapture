246a2ca6d4b72294a5f0e171e23c9ffb92263045
Carbon <robotcreeper13@gmail.com>
2021-01-18 11:41:36 -0600

    Made users install dotnet if they don't have it.

--- AUCapture-WPF/MainWindow.xaml.cs
+++ AUCapture-WPF/MainWindow.xaml.cs
@@ -177,10 +177,76 @@ namespace AUCapture_WPF
             //ApplyDarkMode();
         }
 
+        public async void InstallDotnet()
+        {
+            string DownloadURL = "https://download.visualstudio.microsoft.com/download/pr/c6a74d6b-576c-4ab0-bf55-d46d45610730/f70d2252c9f452c2eb679b8041846466/windowsdesktop-runtime-5.0.1-win-x64.exe";
+            var DownloadProgress =
+                        await context.DialogCoordinator.ShowProgressAsync(context, "Downloading Dotnet", "Percent: 0% (0/0)", isCancelable:false);
+            DownloadProgress.Maximum = 100;
+            using (var client = new WebClient())
+            {
+                var downloadPath = Path.GetTempFileName();
+                client.DownloadProgressChanged += (sender, args) =>
+                {
+                    DownloadProgress.SetProgress(args.ProgressPercentage);
+                    DownloadProgress.SetMessage($"Percent: {args.ProgressPercentage}% ({args.BytesReceived.Bytes().Humanize("#.##")}/{args.TotalBytesToReceive.Bytes().Humanize("#.##")})");
+                };
+                client.DownloadFileCompleted += async (sender, args) =>
+                {
+                    if (args.Error is not null)
+                    {
+                        await DownloadProgress.CloseAsync();
+                        var errorBox = await context.DialogCoordinator.ShowMessageAsync(context, "ERROR",
+                            args.Error.Message, MessageDialogStyle.AffirmativeAndNegative,
+                            new MetroDialogSettings
+                            {
+                                AffirmativeButtonText = "retry",
+                                NegativeButtonText = "cancel",
+                                DefaultButtonFocus = MessageDialogResult.Affirmative
+                            });
+                        if (errorBox == MessageDialogResult.Affirmative)
+                        {
+                            await Task.Factory.StartNew(InstallDotnet, TaskCreationOptions.LongRunning);
+                        }
+                    }
+                    else
+                    {
+                        System.Diagnostics.Process.Start(downloadPath);
+                        await DownloadProgress.CloseAsync();
+                        await context.DialogCoordinator.ShowMessageAsync(context, "Please install dotnet", "Dotnet is required for the next version of AutoMuteUs.", MessageDialogStyle.Affirmative);
+                    }
+                };
+                var downloaderClient = client.DownloadFileTaskAsync(DownloadURL, downloadPath);
+            }
+        }
         public async void Update()
         {
             Version version = new Version(context.Version);
             Version latestVersion = new Version(context.LatestVersion);
+            string strCmdText;
+            strCmdText= "/c dotnet --list-runtimes > \"%TEMP%\\desktopRuntimes.txt\"";
+            var cmd = System.Diagnostics.Process.Start("CMD.exe",strCmdText);
+            string[] lines;
+            await cmd.WaitForExitAsync();
+            try
+            {
+               lines = System.IO.File.ReadAllLines(Path.Join(Path.GetTempPath(), "desktopRuntimes.txt"));
+               if (!lines.Any(x => x.StartsWith("Microsoft.WindowsDesktop.App 5.0.1", StringComparison.InvariantCultureIgnoreCase)))
+               {
+                   InstallDotnet(); //Need to install dotnet
+               }
+               else
+               {
+                   Console.WriteLine("do not need to install dotnet");
+               }
+            }
+            catch (Exception e)
+            {
+                InstallDotnet(); //Need to install dotnet
+            }
+            
+            
+            
 #if PUBLISH
             if (latestVersion.CompareTo(version) > 0)
             {

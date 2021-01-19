f5c74f3de3171f219d6a1b3d0e374e630d792d82
Carbon <robotcreeper13@gmail.com>
2021-01-18 19:28:41 -0600

    Improve string matching

--- AUCapture-WPF/MainWindow.xaml.cs
+++ AUCapture-WPF/MainWindow.xaml.cs
@@ -399,9 +399,9 @@ namespace AUCapture_WPF
             }
             else
             {
-                if (e.Action != PlayerAction.Joined && context.Players.Any(x => x.Name == e.Name || x.Color==e.Color))
+                if (e.Action != PlayerAction.Joined && context.Players.Any(x => String.Equals(x.Name, e.Name, StringComparison.CurrentCultureIgnoreCase) && x.Color==e.Color))
                 {
-                    var player = context.Players.First(x => x.Name == e.Name || x.Color == e.Color);
+                    var player = context.Players.First(x =>  String.Equals(x.Name, e.Name, StringComparison.CurrentCultureIgnoreCase) && x.Color == e.Color);
                     Dispatcher.Invoke((Action) (() =>
                     {
                         switch (e.Action)

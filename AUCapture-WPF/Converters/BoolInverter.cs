5a3fddbaae5a97f8439adffbd36f862e140e8857
Carbon <robotcreeper13@gmail.com>
2021-01-19 20:00:24 -0600

    Get rid of expanders

---
+++ AUCapture-WPF/Converters/BoolInverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AUCapture_WPF.Converters
+{
+    class BoolInverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+            else
+                return !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.Equals(true.ToString()) ?? false;
+        }
+    }
+}

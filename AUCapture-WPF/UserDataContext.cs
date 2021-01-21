4b8b4d7f327af3d8974817aa33272159526af28a
Carbon <robotcreeper13@gmail.com>
2021-01-21 15:36:38 -0600

    Make changes compatible with main repo

--- AUCapture-WPF/UserDataContext.cs
+++ AUCapture-WPF/UserDataContext.cs
@@ -247,13 +247,12 @@ namespace AUCapture_WPF
                 Release latest = new Release();
                 try
                 {
-                    //TODO: latest = client.Repository.Release.GetLatest("automuteus", "amonguscapture").Result;
-                    latest = client.Repository.Release.GetLatest("CarbonNeuron", "AmongUsCapture").Result;
+                    latest = client.Repository.Release.GetLatest("automuteus", "amonguscapture").Result;
 
                 }
                 catch (Exception e)
                 {
-                    //latest = client.Repository.Release.GetLatest("denverquane", "amonguscapture").Result;
+                    latest = client.Repository.Release.GetLatest("denverquane", "amonguscapture").Result;
                 }
                 
                 LatestReleaseAssetURL = latest.Assets.First(x => x.Name == "AmongUsCapture.zip").BrowserDownloadUrl;

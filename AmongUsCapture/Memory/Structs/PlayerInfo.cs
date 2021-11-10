3ff48b4f0a2b60f86b2d9d8ba6c2795b12c9235b
kurokobo <2920259+kurokobo@users.noreply.github.com>
2021-11-11 01:05:16 +0900

    feat: add support for major update on 2021.11.9

--- AmongUsCapture/Memory/Structs/PlayerInfo.cs
+++ AmongUsCapture/Memory/Structs/PlayerInfo.cs
@@ -9,16 +9,25 @@ namespace AmongUsCapture
     public class PlayerInfo
     {
         public byte PlayerId;
-        public String PlayerName;
+
+        public string PlayerName;
         public PlayerColor ColorId;
         public uint HatId;
         public uint PetId;
         public uint SkinId;
+
+        public uint PlayerLevel;
         public bool Disconnected;
+
+        public uint RoleType;
+        public uint RoleTeamType;
+
         public IntPtr Tasks;
         public bool IsImpostor;
         public bool IsDead;
+
         public IntPtr _object; //Assume this always has largest offset
+
         public PlayerInfo(IntPtr baseAddr, ProcessMemory MemInstance, GameOffsets CurrentOffsets) {
             unsafe {
                 var baseAddrCopy = baseAddr;
@@ -27,20 +36,32 @@ namespace AmongUsCapture
                 int size = ((int)Math.Ceiling((decimal) ((intPtrSize + CurrentOffsets.PlayerInfoStructOffsets.ObjectOffset)/8)))*8; //Find the nearest multiple of 8
                 byte[] buffer = MemInstance.Read(baseAddrCopy + last, size);
                 PlayerInfoStructOffsets pOf = CurrentOffsets.PlayerInfoStructOffsets;
+                PlayerOutfitStructOffsets oOf = CurrentOffsets.PlayerOutfitStructOffsets;
+                var outfit = MemInstance.Read<IntPtr>(baseAddrCopy, pOf.OutfitsOffset);
                 fixed (byte* ptr = buffer) {
                     var buffptr = (IntPtr) ptr;
                     PlayerId = Marshal.ReadByte(buffptr, pOf.PlayerIDOffset);
-                    var NamePTR = MemInstance.is64Bit ? (IntPtr) Marshal.ReadInt64(buffptr, pOf.PlayerNameOffset) : (IntPtr) Marshal.ReadInt32(buffptr, pOf.PlayerNameOffset);
-                    PlayerName = NamePTR == IntPtr.Zero ? "" : MemInstance.ReadString(NamePTR, CurrentOffsets.StringOffsets[0], CurrentOffsets.StringOffsets[1]);
-                    ColorId = (PlayerColor)(uint)Marshal.ReadInt32(buffptr, pOf.ColorIDOffset);
-                    HatId = (uint) Marshal.ReadInt32(buffptr, pOf.HatIDOffset);
-                    PetId = (uint) Marshal.ReadInt32(buffptr, pOf.PetIDOffset);
-                    SkinId = (uint) Marshal.ReadInt32(buffptr, pOf.SkinIDOffset);
                     Disconnected = Marshal.ReadByte(buffptr, pOf.DisconnectedOffset) > 0;
                     Tasks = Marshal.ReadIntPtr(buffptr, pOf.TasksOffset);
-                    IsImpostor = Marshal.ReadByte(buffptr, pOf.ImposterOffset) == 1;
-                    IsDead = Marshal.ReadByte(buffptr, pOf.DeadOffset) > 0;
+                    IsDead = Marshal.ReadByte(buffptr, pOf.IsDeadOffset) > 0;
                     _object = Marshal.ReadIntPtr(buffptr, pOf.ObjectOffset);
+
+                    // Read from Role
+                    RoleType = (uint)MemInstance.Read<int>(baseAddrCopy, pOf.RoleTypeOffset);
+                    RoleTeamType = (uint)MemInstance.Read<int>(baseAddrCopy, pOf.RoleTeamTypeOffset);
+                    IsImpostor = RoleTeamType == 1;
+
+                    // Read from PlayerOutfit
+                    PlayerName = MemInstance.ReadString(MemInstance.Read<IntPtr>(outfit, oOf.PlayerNameOffset), CurrentOffsets.StringOffsets[0], CurrentOffsets.StringOffsets[1]);
+                    ColorId = (PlayerColor)(uint)MemInstance.Read<int>(outfit, oOf.ColorIDOffset);
+                    // TODO: Since IDs are changed from enum to string like "hat_police", renaming or mapping existing svgs to string is required
+                    // TODO: As a workaround just fill with 0 as IDs
+                    //HatId = MemInstance.ReadString(MemInstance.Read<IntPtr>(outfit, oOf.HatIDOffset), CurrentOffsets.StringOffsets[0], CurrentOffsets.StringOffsets[1]);
+                    //PetId = MemInstance.ReadString(MemInstance.Read<IntPtr>(outfit, oOf.PetIDOffset), CurrentOffsets.StringOffsets[0], CurrentOffsets.StringOffsets[1]);
+                    //SkinId = MemInstance.ReadString(MemInstance.Read<IntPtr>(outfit, oOf.SkinIDOffset), CurrentOffsets.StringOffsets[0], CurrentOffsets.StringOffsets[1]);
+                    HatId = 0;
+                    PetId = 0;
+                    SkinId = 0;
                 }
             }
         }
